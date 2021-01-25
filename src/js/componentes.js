5742553167c33fa03459c0db8c1552dcefd0ee01
lsauquillo <lsauquillo@gmail.com>
2021-01-25 17:13:30 +0100

    Instalado webpack-dev-server

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,6 +1,6 @@
 export const saludar = (nombre) => {
   console.log('Creando una etiqueta h1 con el saludo...');
   const h1 = document.createElement('h1');
-  h1.innerText = `Hola, soy ${nombre}`;
+  h1.innerText = `Hola, soy ${nombre} desde dev-server`;
   document.body.append(h1)
 }
